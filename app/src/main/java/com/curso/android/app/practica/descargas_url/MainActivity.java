1b4066d2c549734c58d840d8592b271a39856246
karinacolmann <karinacolmann1812@gmail.com>
2023-12-01 02:03:53 -0300

    Commit inicial

---
+++ app/src/main/java/com/curso/android/app/practica/descargas_url/MainActivity.java
@@ -0,0 +1,23 @@
+package com.curso.android.app.practica.descargas_url;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Free(View view) {
+        Intent intent1 = new Intent(this, MainActivity2.class);
+        startActivity(intent1);
+    }
+
+
+}
